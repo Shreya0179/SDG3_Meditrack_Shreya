cdbd4507b654632914e5034f6ccaa71ddb3f2a08
Shreya <shreya.2428cseaiml25@kiet.edu>
2026-07-15 14:34:28 +0530

    Configured environment variables for local and production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -61,7 +61,7 @@ export const loginUser = async (req, res) => {
 
     const token = jwt.sign(
       { id: user._id },
-      "meditracksecret",
+      process.env.JWT_SECRET,
       {
         expiresIn: "1d",
       }
